6d20c55722bf0b2c7f983ff0df929c5c164ebfd5
Sectusius <ianpetraccaro@gmail.com>
2023-07-17 09:49:42 -0300

    Added logic to update about section, but it's not saving to db

--- portfolio/src/about/about.tsx
+++ portfolio/src/about/about.tsx
@@ -3,22 +3,51 @@ import './about.css';
 import { useEffect } from "react";
 import SectionData from '../types/sectionData';
 import SectionService from '../services/sectionService';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencil } from '@fortawesome/free-solid-svg-icons';
 
 const About = () => {
     const [aboutSection, setAboutSection] = useState<SectionData | null>(null);
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+    const [updatedContent, setUpdatedContent] = useState<SectionData | null>(null);
+
+    const fetchSectionData = async () => {
+        try {
+            const sectionData = await SectionService.get('About');
+            setAboutSection(sectionData.data);
+            console.log(sectionData);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    };
+
     useEffect(() => {
-        const fetchSectionData = async () => {
-            try {
-                const sectionData = await SectionService.get('About');
-                setAboutSection(sectionData.data);
-                console.log(sectionData);
-            }
-            catch (error) {
-                console.log(error);
-            }
-        };
+
         fetchSectionData();
     }, []);
+
+    const handleEditClick = () => {
+        setUpdatedContent(aboutSection);
+        setIsEditing(true);
+    };
+
+    const handleSaveClick = async () => {
+        try{
+            console.log(updatedContent);
+            const updatedSection = await SectionService.update(updatedContent!.title, updatedContent!);
+            console.log(updatedSection);
+            fetchSectionData();
+            setIsEditing(false);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleCancelClick = () => {
+        setIsEditing(false);
+    };
+
     return (
         
         <div className="about">
@@ -27,9 +56,24 @@ const About = () => {
             </div>
             <div className="about-content">
                 <h1>Acerca de mi</h1>
-                <p style={{whiteSpace:"pre-line"}}>
-                    {aboutSection?.content}
-                </p>
+                {isEditing ? (
+                    <>
+                        <textarea
+                            value={updatedContent?.content}
+                            onChange={(e) => setUpdatedContent({ ...updatedContent!, content: e.target.value })}
+                            style={{width:'100%', height:'200px', color:"white"}}
+                        />
+                        <button className='btn btn-primary' onClick={handleSaveClick}>Guardar</button>
+                        <button className='btn btn-warning' onClick={handleCancelClick}>Cancelar</button>
+                    </>) : (
+                        <>
+                        <p style={{whiteSpace:"pre-line", display:'inline-block'}}>
+                            {aboutSection?.content}
+                            
+                        </p>
+                            <FontAwesomeIcon icon={faPencil} onClick={handleEditClick} style={{cursor:'pointer'}}></FontAwesomeIcon>
+                        </>
+                    )}
             </div>
         </div>
     )
